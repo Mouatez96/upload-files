69747173d0b34e993d9f2515c57f9e59aec9e528
Mouatez96 <khalfaouimouatezbillah@gmail.com>
2023-10-22 23:36:19 +0200

    add multiple upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MessageService} from "primeng/api";
 import {FileUploadEvent, UploadEvent} from "primeng/fileupload";
 import {AngularFireStorage} from "@angular/fire/compat/storage";
+import {forkJoin} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -15,14 +16,26 @@ export class AppComponent {
   constructor(private messageService: MessageService,
               private _angularStorage: AngularFireStorage) {}
 
-  async onUpload(event: FileUploadEvent) {
+  async onUploadOne(event: FileUploadEvent) {
     this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded with Basic Mode' });
     const file = event.files[0];
     console.log(file)
     if (file) {
       const path = `files/${file.name}`;
-      const fileUpload = this._angularStorage.upload(path, file);
-      console.log(path)
+      const fileUpload = await this._angularStorage.upload(path, file);
+      console.log(fileUpload)
     }
+  }
+
+  async onUpload(event: FileUploadEvent) {
+    console.log(event)
+    const promises: any[]= [];
+    event.files.forEach(file => promises.push(this._angularStorage.upload(`multiple/${file.name}`, file)));
+    const observable = forkJoin([promises]);
+    observable.subscribe({
+      next: value => console.log(value),
+      complete: () => console.log("all done")
+    })
+
   }
 }
